96cdcd515dbffb22001e06e43932ee14c1e8e4ad
Ziad Hatem <ziadhatem2022@gmail.com>
2024-09-14 15:39:57 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo-production-f568.up.railway.app",
-			},
-		],
-	},
+  distDir: "dist",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "digitalhippo-production-f568.up.railway.app",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
